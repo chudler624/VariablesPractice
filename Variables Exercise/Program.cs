a800729c34b72168012b0d62f75b8c6e1d1ed181
Casey Hudler <chudler624@gmail.com>
2022-08-05 12:46:35 -0400

    variables practice

---
+++ Variables Exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string myName = "Casey";
+int myAge = 33;
+char myGrade = 'a';
+double mySalary = 100000;
+string dreamJob = "software developer";
+string mySchool = "TrueCoders";
+
+
+Console.WriteLine($"Hi, my name is {myName}. I'm {myAge} years old. Right now, I'm attending {mySchool}.");
+Console.WriteLine($"I'm studying really hard so i can an {myGrade} and then get as job as a {dreamJob} and i can make {mySalary}.");
